c8e6dec04245382a0d5870b9074ab6462ba59d7d
brunoaraujodeoliveira <brunimaraujodeoliveira@gmail.com>
2021-03-23 09:58:03 -0300

    updates to deploy to heroku

--- src/main/java/br/com/sgm/gateway/config/CorsMappingConfig.java
+++ src/main/java/br/com/sgm/gateway/config/CorsMappingConfig.java
@@ -13,7 +13,7 @@ public class CorsMappingConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
+				registry.addMapping("/**").allowedOrigins("https://tcc-sgm.herokuapp.com");
 			}
 		};
 	}
